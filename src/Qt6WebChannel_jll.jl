ec7e2dbab724e7841b31d29dff1bd089bb15d6e9
jlbuild <juliabuildbot@gmail.com>
2025-12-09 12:10:09 +0000

    Qt6WebChannel_jll build 6.8.2+0

--- src/Qt6WebChannel_jll.jl
+++ src/Qt6WebChannel_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6WebChannel")
-JLLWrappers.@generate_main_file("Qt6WebChannel", UUID("83f1a98f-4f9b-584f-8f95-63c67b3a1da2"))
+JLLWrappers.@generate_main_file("Qt6WebChannel", Base.UUID("83f1a98f-4f9b-584f-8f95-63c67b3a1da2"))
 end  # module Qt6WebChannel_jll
